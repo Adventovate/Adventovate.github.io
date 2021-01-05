67b103bb0811a64446670d7f6a241697f6119cb2
vikeshdass <Findwork@007>
2021-01-05 21:48:41 +0530

    Updates

---
+++ precache-manifest.5e0b262db89e420315f88ddda807f0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990b69a41c8f1b0542f734d96caba0a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0cddf04945e6c6ff18",
+    "url": "/static/css/main.6d9a06f1.chunk.css"
+  },
+  {
+    "revision": "2d5cec345dbb9fd8e92f",
+    "url": "/static/js/2.4628c5fb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4628c5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0cddf04945e6c6ff18",
+    "url": "/static/js/main.e8069ba9.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
